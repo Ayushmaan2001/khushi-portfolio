2f791e0acf7801ca2b84429cf1ee67067785cfc5
Ayushmaan2001 <ayushmaanjangid2002@gmail.com>
2022-05-13 23:27:08 +0530

    contact us

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 export default function ContactUs() {
+  const mail =
+    "https://mail.google.com/mail/?view=cm&fs=1&to=viratrohitmsd2001@gmail.com";
   return (
-    <div>ContactUs</div>
-  )
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        You Can Contact Us on{" "}
+        <a href={mail} target="_blank">
+          Mail Us
+        </a>
+        {" "}or you can whatsapp us on 9013069266
+      </Container>
+    </React.Fragment>
+  );
 }
