69b8db8f58e43b26fe6838f198feea27b62bd256
aryan jangid <aryan100jangid@gmail.com>
2022-05-14 23:10:13 +0530

    Add Contact Us page

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,21 +1,87 @@
 import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
+import "./Contact.css";
 
 export default function ContactUs() {
-  const mail =
-    "https://mail.google.com/mail/?view=cm&fs=1&to=viratrohitmsd2001@gmail.com";
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="xl">
-        You Can Contact Us on{" "}
-        <a href={mail} target="_blank">
-          Mail Us
-        </a>
-        {" "}or you can whatsapp us on 9013069266
-      </Container>
+      <div class="container">
+        <span class="big-circle"></span>
+        <img src="img/shape.png" class="square" alt="" />
+        <div class="form">
+          <div class="contact-info">
+            <h3 class="title">Contact Us</h3>
+            <p class="text">Be the star of the show with our makeup</p>
+
+            <div class="info">
+              <div class="information">
+                <img src="./img/location.png" class="icon" alt="" />
+                <p>Shahadara, Delhi - 110093</p>
+              </div>
+              <div class="information">
+                <img src="./img/email.png" class="icon" alt="" />
+                <p>khushujangid12@gmail.com</p>
+              </div>
+              <div class="information">
+                <img src="./img/phone.png" class="icon" alt="" />
+                <p>9663846897</p>
+              </div>
+            </div>
+
+            <div class="social-media">
+              <p>Connect with us :</p>
+              <div class="social-icons">
+                <a href="https://www.instagram.com/_khushijangid.mua_/">
+                  <i class="fab fa-instagram"></i>
+                </a>
+                <a href="#">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="contact-form">
+            <span class="circle one"></span>
+            <span class="circle two"></span>
+
+            <form action="index.html" autocomplete="off">
+              <h3 class="title">Contact us</h3>
+              <div class="input-container">
+                <input
+                  type="text"
+                  name="name"
+                  class="input"
+                  placeholder="Username"
+                />
+              </div>
+              <div class="input-container">
+                <input
+                  type="email"
+                  name="email"
+                  class="input"
+                  placeholder="Email"
+                />
+              </div>
+              <div class="input-container">
+                <input
+                  type="tel"
+                  name="phone"
+                  class="input"
+                  placeholder="Phone"
+                />
+              </div>
+              <div class="input-container textarea">
+                <textarea
+                  name="message"
+                  class="input"
+                  placeholder="Type your message here..."
+                ></textarea>
+              </div>
+              <input type="submit" value="Send" class="btn" />
+            </form>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
