c187d0132b9c2bcc605fe67907aa5bc753c0c155
Ayushmaan2001 <ayushmaanjangid2002@gmail.com>
2022-05-14 22:58:57 +0530

    home_new

--- src/Components/ImageCard.jsx
+++ src/Components/ImageCard.jsx
@@ -6,7 +6,22 @@ import Typography from "@mui/material/Typography";
 
 export default function ImageCard({ images }) {
   return (
-    <Card sx={{ maxWidth: 345, ml: 2, mb: 2, mt: 2, display: "inline-flex" }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+        mr: 1,
+        ml: 1,
+        mb: 2,
+        mt: 2,
+        display: {
+          xl: "inline-block",
+          lg: "inline-block",
+          md: "inline-block",
+          sm: "inline-block",
+          xs: "block",
+        },
+      }}
+    >
       <CardMedia component="img" height="400" image={images} />
     </Card>
   );
