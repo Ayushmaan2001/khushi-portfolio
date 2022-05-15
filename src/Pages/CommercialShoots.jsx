617c7f82643357dbf3da21a4ca6fbcc47dfd8623
Ayushmaan2001 <ayushmaanjangid2002@gmail.com>
2022-05-15 12:20:25 +0530

    commercial_shoots

--- src/Pages/CommercialShoots.jsx
+++ src/Pages/CommercialShoots.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import UseImages from '../hooks/useImages';
+import ImageCard from '../Components/ImageCard';
 
 const CommercialShoots = () => {
+  const { commercial_shoots } = UseImages();
   return (
     <div>
-      Commercial Shoots
+      {commercial_shoots.map((images) => {
+        return <ImageCard images={images.img} />;
+      })}
     </div>
   );
 }
---
+++ src/hooks/useImages.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const UseImages = () => {
+    const commercial_shoots = [
+      {
+        img: require("../Comercial shoot/069A51331643469002558.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/069A51341643469002558.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/069A77991643650612761-01.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/069A77991643650612761.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/069A78001643650612761-01.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/069A78011643650612761.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/069A78391643650612766.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/069A78491643650612767 (1).jpg"),
+      },
+      {
+        img: require("../Comercial shoot/20220323_204045.jpg"),
+      },
+      {
+        // img: require("../Comercial shoot/MegaanShoot_29 March5539 (1).jpg"),
+      },
+      {
+        img: require("../Comercial shoot/Megaan Shoot_29 March5540.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/Megaan Shoot_29 March6162.jpg"),
+      },
+      {
+        // img: require("../Comercial shoot/MegaanShoot_29 March6172 (1).jpg"),
+      },
+      {
+        img: require("../Comercial shoot/PicsArt_01-28-12.23.12.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/PicsArt_01-28-12.23.57.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/PicsArt_01-28-12.24.55.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/PicsArt_01-28-12.28.26.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/PicsArt_01-28-12.29.30.jpg"),
+      },
+      {
+        img: require("../Comercial shoot/PicsArt_01-28-12.32.11.jpg"),
+      },
+    ];
+
+    const creative_makeup = [];
+    return {commercial_shoots,creative_makeup};
+}
+
+export default UseImages;
